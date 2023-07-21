dac895d68aa7fc88c519cc83fb6b1e456d35b531
RoxMayra <mayrapuma2023@gmail.com>
2023-07-21 09:41:42 -0500

    agregar

--- src/component/C03contador2.js
+++ src/component/C03contador2.js
@@ -5,18 +5,18 @@ const C03ontador2 = () => {
     const [valor, setValor] = useState(0);
     const [numero, setNumero] = useState(0);
     //Definiendo función flecha que incremente un valor 
-    const IncrementoDos = ()=>{
+    const IncrementoDos = () => {
         setNumero(numero + 1);
         console.log("Mensaje de contador");
     }
     return (
         <Fragment>
             <button onClick={() =>setValor (valor+1)}>Contador 1</button>
-            <span>Incremento de contador 1: {valor} </span>
+            <span>Incremento de contador 1: {valor}</span>
             <hr/>
-            <button onClick= {IncrementoDos}>Contador 2</button>
+            <button onClick={IncrementoDos}>Contador 2</button>
             <span>Incremento de contador 2: {numero}</span>
         </Fragment>
-);
-
+    );
+}
 export default C03ontador2;
